b551b0c3a6a0559961cda9a0634b6892b142da23
Shubham <shubhamraihero2015@gmail.com>
2024-10-17 18:50:15 +0530

    cors update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.connect(config.connectionString)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.log('MongoDB connection error:', err)
-    process.exit()
+    process.exit(1)
   });
 
 const User = require("./models/user.model");
@@ -30,18 +30,18 @@ const allowedOrigins = [
 ];
 
 // Apply CORS middleware
-app.use(
-  cors({
-    origin: '*',
-    headers:{
-      "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Credentials":true
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true); // Allow the origin
+    } else {
+      callback(new Error('Not allowed by CORS')); // Block the origin
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // Enable pre-flight across-the-board
 app.options("*", cors());
