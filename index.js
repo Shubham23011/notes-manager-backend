42da11fbf23f98763b0f4748366d688c10fc4af4
Shubham <shubhamraihero2015@gmail.com>
2024-10-17 18:35:38 +0530

    cors update

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const allowedOrigins = [
 // Apply CORS middleware
 app.use(
   cors({
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
